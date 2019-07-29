21842c72e957d2278750b8016430507e54836544
Tim Marwick <triyuga@gmail.com>
2019-07-29 15:30:58 +1000

    Create upload-to-azure-storage.sh

---
+++ scripts/upload-to-azure-storage.sh
@@ -0,0 +1,2 @@
+# Use this syntax (quotes) when inputting in DevOps CD pipeline Azure CLI task 
+az storage blob upload-batch --account-name STORAGE_ACCOUNT_NAME --account-key STORAGE_ACCOUNT_KEY --destination "$web" --source "./"
